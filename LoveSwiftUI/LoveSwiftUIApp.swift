33210ecacc6b4be455914bef02e5bb3f872ff1fd
sakurakotubaki <gitonly543@gmail.com>
2026-02-12 21:38:45 +0900

    Initial Commit

---
+++ LoveSwiftUI/LoveSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveSwiftUIApp.swift
+//  LoveSwiftUI
+//
+//  Created by 橋本純一 on 2026/02/12.
+//
+
+import SwiftUI
+
+@main
+struct LoveSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
