4a8a042f37cbc471808fe8bfb33b7c717d66d724
sakurakotubaki <gitonly543@gmail.com>
2026-02-13 06:54:21 +0900

    added: PhoneNumber

---
+++ LoveSwiftUI/UserListView.swift
@@ -0,0 +1,18 @@
+//
+//  UserListView.swift
+//  LoveSwiftUI
+//
+//  Created by 橋本純一 on 2026/02/13.
+//
+
+import SwiftUI
+
+struct UserListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserListView()
+}
---
+++ LoveSwiftUI/YouTubeTraceView.swift
@@ -0,0 +1,18 @@
+//
+//  YouTubeTraceView.swift
+//  LoveSwiftUI
+//
+//  Created by 橋本純一 on 2026/02/12.
+//
+
+import SwiftUI
+
+struct YouTubeTraceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouTubeTraceView()
+}
